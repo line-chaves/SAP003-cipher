3fc3871c67d08b4c2c4c5062ba79fe8f13df2dbd
alinechaves <line.limachaves@gmail.com>
2019-08-29 23:19:59 -0300

    Alteração do README

--- src/cipher.js
+++ src/cipher.js
@@ -11,17 +11,33 @@ function encode(offset, mensagem) {
     let codigoAsc = mensagem.charCodeAt(i);
     //inicio da condição.
     //letra maiuscula
-    if (codigoAsc >= 65 && codigoAsc <= 90) {
-      retorno += String.fromCharCode(((mensagem.charCodeAt(i) - 65 + offset) % 26) + 65);
+    if (offset > 0) {
+      if (codigoAsc >= 65 && codigoAsc <= 90) {
+        retorno += String.fromCharCode(((codigoAsc - 65 + offset) % 26) + 65);
+      }
+      //letra minuscula
+      else if (codigoAsc >= 97 && codigoAsc <= 122) {
+        retorno += String.fromCharCode(((codigoAsc - 97 + offset) % 26) + 97);
+      }
+      //caractere especial e numeros
+      else {
+        (codigoAsc >= 32 && codigoAsc <= 64);
+        retorno += String.fromCharCode(codigoAsc);
+      }
     }
-    //letra minuscula
-    else if (codigoAsc >= 97 && codigoAsc <= 122) {
-      retorno += String.fromCharCode(((codigoAsc - 97 + offset) % 26) + 97);
-    }
-    //caractere especial e numeros
     else {
-      (codigoAsc >= 32 && codigoAsc <= 64);
-      retorno += String.fromCharCode(codigoAsc);
+      (offset < 0);
+      if (codigoAsc >= 65 && codigoAsc <= 90) {
+        retorno += String.fromCharCode(((codigoAsc - 90 + offset) % 26) +90);
+      }
+      //letra minuscula
+      if (codigoAsc >= 97 && codigoAsc <= 122) {
+        retorno += String.fromCharCode(((codigoAsc - 122 + offset) % 26) +122);
+      }
+      //caractere especial e numeros
+      {
+        retorno = mensagem;
+      }
     }
   }
   return retorno;
@@ -29,22 +45,38 @@ function encode(offset, mensagem) {
 //função decode para letras 
 function decode(offset, mensagem) {
   let result = "";
-//laço de repetição 
+  //laço de repetição 
   for (let j = 0; j < mensagem.length; j++) {
     let CodAsc = mensagem.charCodeAt(j);
     //inicio da condição 
     //letra maiuscula
-    if (CodAsc >= 65 && CodAsc <= 90) {
-      result += String.fromCharCode(((CodAsc - 90 - offset) % 26) + 90);
+    if (offset>0) {
+      if (CodAsc >= 65 && CodAsc <= 90) {
+        result += String.fromCharCode(((CodAsc - 90 - offset) % 26) + 90);
+      }
+      //letra minuscula
+      else if (CodAsc >= 97 && CodAsc <= 122) {
+        result += String.fromCharCode(((CodAsc - 122 - offset) % 26) + 122);
+      }
+      //caractere especial e numeros 
+      else {
+        (CodAsc >= 32 && CodAsc <= 64);
+        result += String.fromCharCode(CodAsc);
+      }
     }
-    //letra minuscula
-    else if (CodAsc >= 97 && CodAsc <= 122) {
-      result += String.fromCharCode(((CodAsc - 122 - offset) % 26) + 122);
-    }
-    //caractere especial e numeros 
     else {
-      (CodAsc >= 32 && CodAsc <= 64);
-      result += String.fromCharCode(CodAsc);
+      (offset<0);
+      if (CodAsc >= 65 && CodAsc <= 90) {
+        result += String.fromCharCode(((CodAsc - 65 - offset) % 26) + 65);
+      }
+      //letra minuscula
+      if (CodAsc >= 97 && CodAsc <= 122) {
+        result += String.fromCharCode(((CodAsc - 97 - offset) % 26) + 97);
+      }
+      //caractere especial e numeros
+      {
+        result = mensagem;
+      }
     }
   }
   return result;
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -8,10 +8,19 @@ describe("cipher", () => {
     it("should be a function", () => {
       assert.equal(typeof cipher.encode, "function");
     });
-    it("should return \"HIJKLMNOPQRSTUVWXYZABCDEFG\" for \"ABCDEFGHIJKLMNOPQRSTUVWXYZ\" with offset 33", () => {
+    it("should return \"HIJKLMNOPQRSTUVWXYZABCDEFG\" for \"ABCDEFGHIJKLMNOPQRSTUVWXYZ\" with offset  33", () => {
       assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
     });
+    it("should return \"TUVWXYZABCDEFGHIJKLMNOPQRS\" for \"ABCDEFGHIJKLMNOPQRSTUVWYXZ\" with offset - 33", () => {
+      assert.equal(cipher.encode(-33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "TUVWXYZABCDEFGHIJKLMNOPQRS");
+    });
     //letras minusculas
+    it("should return \"tuvwxyzabcdefghijklmnopqrs\" for \"abcdefghijklmnopqrstuvwxyz\" with offset - 33", () => {
+      assert.equal(
+        cipher.encode(-33, "abcdefghijklmnopqrstuvwxyz"),
+        "tuvwxyzabcdefghijklmnopqrs"
+      );
+    });
     it("should return \"hijklmnopqrstuvwxyzabcdefg\" for \"abcdefghijklmnopqrstuvwxyz\" with offset 33", () => {
       assert.equal(
         cipher.encode(33, "abcdefghijklmnopqrstuvwxyz"),
@@ -19,7 +28,7 @@ describe("cipher", () => {
       );
     });
     //caracteres não alfabeticos
-    it("should return \" !@\" for \" !@\" with offset 33", () => {
+    it("should return \" !@\" for \" !@\"", () => {
       assert.equal(cipher.encode(33, " !@"), " !@");
     });
   });
@@ -30,10 +39,18 @@ describe("cipher", () => {
       assert.equal(typeof cipher.decode, "function");
     });
 
+    it("should return \"HIJKLMNOPQRSTUVWXYZABCDEFG\" for \"ABCDEFGHIJKLMNOPQRSTUVWXYZ\" with offset - 33", () => {
+      assert.equal(cipher.decode(-33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
+    });
     it("should return \"ABCDEFGHIJKLMNOPQRSTUVWXYZ\" for \"HIJKLMNOPQRSTUVWXYZABCDEFG\" with offset 33", () => {
       assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     });
     //decode minuscula
+    it("should return \"hijklmnopqrstuvwxyzabcdefg\" for \"abcdefghijklmnopqrstuvwxyz\" with offset -33", () => {
+      assert.equal(
+        cipher.decode(-33, "abcdefghijklmnopqrstuvwxyz"), "hijklmnopqrstuvwxyzabcdefg"
+      );
+    });
     it("should return \"abcdefghijklmnopqrstuvwxyz\" for \"hijklmnopqrstuvwxyzabcdefg\" with offset 33", () => {
       assert.equal(
         cipher.decode(33, "hijklmnopqrstuvwxyzabcdefg"),
@@ -42,7 +59,7 @@ describe("cipher", () => {
     });
 
     //decode caractere não alfabetico
-    it("should return \" !@\" for \" !@\" with offset 33", () => {
+    it("should return \"!@\" para \" !@\"", () => {
       assert.equal(cipher.decode(33, " !@"), " !@");
     });
   });
